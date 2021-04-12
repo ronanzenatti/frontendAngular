6b443ac54c9cc519adb85c86d5e017fd4f87108a
Ronan Adriel Zenatti <ronan.zenatti@etec.sp.gov.br>
2021-04-12 03:55:33 -0300

    Commit inicial

---
+++ src/app/components/produtos/atualizar-produto/atualizar-produto.component.ts
@@ -0,0 +1,42 @@
+import { ProdutosService } from './../../../services/produtos.service';
+import { IProduto } from './../../../model/IProduto.model';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-atualizar-produto',
+  templateUrl: './atualizar-produto.component.html',
+  styleUrls: ['./atualizar-produto.component.css']
+})
+export class AtualizarProdutoComponent implements OnInit {
+
+  produto: IProduto = {
+    nome: null,
+    validade: null,
+    preco: null
+  };
+
+  constructor(
+    private produtosService: ProdutosService,
+    private router: Router,
+    private activatedRouter: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = Number(this.activatedRouter.snapshot.paramMap.get('id'));
+    this.produtosService.buscarPorId(id).subscribe(retorno => {
+      this.produto = retorno;
+    });
+  }
+
+  salvarProduto(): void {
+    this.produtosService.atualizar(this.produto).subscribe(retorno => {
+      this.produto = retorno;
+      this.produtosService.exibirMensagem(
+        'SISTEMA',
+        `${this.produto.nome} foi atualizado com sucesso.`,
+        'toast-success'
+      );
+      this.router.navigate(['/produtos']);
+    });
+  }
+}
